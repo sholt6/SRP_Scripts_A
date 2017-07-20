acd159242381922244f55de3f57c0bf06a80befa
sholt6 <sam_holt30@hotmail.co.uk>
2017-07-20 15:42:42 +0100

    Add files via upload

---
+++ Pipeline_6/rsem-prepare-reference_command_rn6.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+rsem-prepare-reference --gtf /home/sjrh2/Steered/Rattus_norvegicus/Ensembl/Rnor_6.0/Annotation/Genes/genes2.gtf --bowtie-path /home/sjrh2/Tuxedo/bowtie0.12.7 /home/sjrh2/Steered/Rattus_norvegicus/Ensembl/Rnor_6.0/Sequence/Chromosomes /home/sjrh2/Steered/RSEM_reference_rn6/rn6_rsem_ref
