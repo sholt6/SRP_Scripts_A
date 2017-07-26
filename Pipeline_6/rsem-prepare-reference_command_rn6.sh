d744d8de8db57a93f7b31db0c8784ba3a0a0fded
sholt6 <sam_holt30@hotmail.co.uk>
2017-07-26 13:14:24 +0100

    Update rsem-prepare-reference_command_rn6.sh

--- Pipeline_6/rsem-prepare-reference_command_rn6.sh
+++ Pipeline_6/rsem-prepare-reference_command_rn6.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
+#This command was used to build a reference transcriptome using RSEM and Bowtie, for use in quantifying transcripts
 rsem-prepare-reference --gtf /home/sjrh2/Steered/Rattus_norvegicus/Ensembl/Rnor_6.0/Annotation/Genes/genes2.gtf --bowtie-path /home/sjrh2/Tuxedo/bowtie0.12.7 /home/sjrh2/Steered/Rattus_norvegicus/Ensembl/Rnor_6.0/Sequence/Chromosomes /home/sjrh2/Steered/RSEM_reference_rn6/rn6_rsem_ref
