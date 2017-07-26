08e9b558bf41eff2b062d0c5491ad703b588f273
sholt6 <sam_holt30@hotmail.co.uk>
2017-07-26 13:12:22 +0100

    Update gtf_edit.pl

--- Pipeline_6/gtf_edit.pl
+++ Pipeline_6/gtf_edit.pl
@@ -1,11 +1,26 @@
 #!/usr/bin/perl
-#Having identified unplaced scaffolds in EnsEMBL Rnor_6.0 genes.gtf, this script was used to remove certain offending sequences. It then appends the ERCC annotation file to the newly created genes2.gtf.
+#Having identified unplaced scaffolds in EnsEMBL Rnor_6.0 genes.gtf, this script was used to remove certain offending sequences. 
+#Leaving these sequences in trips up RSEM, as it requires that every chromosome referred to in the annotation have a corresponding chromosome.fasta
+#It then appends the ERCC annotation file to the newly created genes2.gtf.
 use strict;
 use warnings;
 
-open (IN, "<genes.gtf") or die "Could not find genes.gtf";
-open (OUT, ">genes2.gtf");
-open (CUT, ">genes_cut.gtf");
+unless (scalar(@ARGV) == 1) {
+	print "Usage: perl gtf_edit.pl <input GTF>\n";
+	exit;
+}
+
+my $file = $ARGV[0];
+
+#Removes a .gtf extension from an input filename, for processing purposes. If the extension is anything other than .gtf, the script will fail. 
+if ($file =~ /\.gtf/) {
+	$file = $`;
+}
+
+#The original file is left intact and a new one is created. The cut annotations are sent to genes_cut.gtf for reviewing.
+open (IN, "<$file.gtf") or die "Could not find $file.gtf";
+open (OUT, ">$file" . "2.gtf");
+open (CUT, ">$file\_cut.gtf");
 
 my $cutot;
 my $keptot;
@@ -24,6 +39,7 @@ while (<IN>) {
 
 close IN; close OUT; close CUT;
 
+#These data can be useful as an indication of whether the script has worked as intended, or for debugging
 print "$keptot lines kept\n$cutot lines cut\n";
 
-system "cat ERCC92.gtf >> genes2.gtf";
+system "cat ERCC92.gtf >> $file" . "2.gtf";
