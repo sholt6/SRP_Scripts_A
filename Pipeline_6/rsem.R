bac9c8b4bfbf3e9de4a2459ec55bbf6746e96a6e
sholt6 <sam_holt30@hotmail.co.uk>
2017-07-26 13:19:35 +0100

    Update rsem.R

--- Pipeline_6/rsem.R
+++ Pipeline_6/rsem.R
@@ -8,10 +8,11 @@ library(edgeR)
 
 rm(list=ls())
 
+###Must uncomment desired input
 #name <- "rn4_trimmed"
 #name <- "rn4_untrimmed"
 #name <- "rn6_trimmed"
-name <- "rn6_untrimmed"
+#name <- "rn6_untrimmed"
 
 rawdata <- read.delim(paste('/home/sjrh2/Steered/edgeR/',name,'.tsv', sep =""), header = TRUE, sep = "\t", row.names = 1)
 
@@ -19,6 +20,7 @@ group <- factor(c(1,2,2,1,2))
 
 dge <- DGEList(counts=rawdata, group=group)
 
+#Next two lines remove any genes which are expressed in <= 2 rats
 keep <- rowSums(cpm(dge) > 0) >=1
 dge <- dge[keep, , keep.lib.sizes=FALSE]
 
@@ -40,7 +42,7 @@ treat$table$FC <- 2^(abs(treat$table$logFC))
 treat$table$FC <- ifelse(treat$table$logFC <0, treat$table$FC*(-1), treat$table$FC*1)
 
 
-## Save QLF Results
+### Save QLF Results
 write.table(qlf$table, file = paste("/home/sjrh2/Steered/edgeR/Results/",name,"_QLF_logFC", sep = ""), sep="\t", row.names=TRUE, col.names = NA)
 write.table(summary(decideTests(qlf, adjust.method = "none")), file = paste("/home/sjrh2/Steered/edgeR/Results/",name,"_QLF_summary", sep = ""), sep="\t", row.names=TRUE, col.names = NA)
 
@@ -58,7 +60,7 @@ dev.off()
 
 
 
-## Save Treat Results
+### Save Treat Results
 write.table(treat$table, file = paste("/home/sjrh2/Steered/edgeR/Results/",name,"_Treat_logFC", sep = ""), sep="\t", row.names=TRUE, col.names = NA)
 write.table(summary(decideTests(treat, adjust.method = "none")), file = paste("/home/sjrh2/Steered/edgeR/Results/",name,"_Treat_summary", sep = ""), sep="\t", row.names=TRUE, col.names = NA)
 
@@ -73,5 +75,3 @@ plot(treat$table$logFC, -log10(treat$table$PValue), pch=19, cex=0.2, col=ifelse(
 abline(h=1.3, col="blue")
 abline(h=2, col="green")
 dev.off()
-
-#treat$table[c("Myc","Fzd4","Tlcd1","Xiap"),]
